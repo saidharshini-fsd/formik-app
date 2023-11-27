14fbe76fa0792d21175d0ec1f9cadf02007d2c43
saidharshini-fsd <saidharshini13@gmail.com>
2023-11-27 20:30:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import BookDetails from './components/BookDetails';
+import AddBook from './components/AddBooks';
+import EditBook from './components/EditBook';
+import { AppProvider } from './components/Context/AppProvider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/edit/:id" element={<EditBook />} />
+          <Route path="/book/add" element={<AddBook />} />
+          <Route path="/book" element={<BookDetails />} />
+        </Routes>
+      </Router>
+      </AppProvider>
     </div>
   );
 }
---
+++ src/components/Data/data.js
@@ -0,0 +1,54 @@
+export const data =[
+    {
+        id : "1",
+        name : "Atomic Habits: the life-changing million",
+        image: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+        author : " James Clear",
+        description : "Transform your life with tiny changes in behaviour, starting now."
+    },
+    {
+        id : "2",
+        name : "Ikigai: The Japanese secret to a long and happy life",
+        image: "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg",
+        author : " Francesc Miralles",
+        description : "[The] Japanese art of ikigai ...Its basic message is about 'authentic livin'."
+    },
+    {
+        id : "3",
+        name : "Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That The Poor And Middle Class Do Not!",
+        image: "https://m.media-amazon.com/images/I/51Hfv2MfNGL._SX331_BO1,204,203,200_.jpg",
+        author : " Robert T. Kiyosaki",
+        description : "Rich Dad Poor Dad by Robert Kiyosaki and Sharon Lechter is a book that came out in 1997 and focuses on the importance of financial literacy from an early age."
+    },
+    {
+        id : "4",
+        name : "Dopamine Detox : A Short Guide to Remove Distractions and Get Your Brain to Do Hard Things",
+        image: "https://m.media-amazon.com/images/I/413tVFk--xS.jpg",
+        author : " thibaut meurisse",
+        description : "Dopamine Detox will help you lower your level of stimulation and regain focus in 48 hours or less,so that you can tackle your key tasks."
+    },
+    {
+        id : "5",
+        name : "The 5 AM Club",
+        image: "https://m.media-amazon.com/images/I/41MC15v6UIL._SX332_BO1,204,203,200_.jpg",
+        author : " Robin Sharma",
+        description : "Part manifesto for mastery, part playbook for genius-grade productivity and part companion for a life lived beautifully, the 5 am club is a work that will transform your life."
+    },
+    {
+      id:"6",
+      name:"To Kill a Mockingbird",
+      image:"https://cdn.lifehack.org/wp-content/uploads/2022/11/41j-s9fHJcL._SL500_.webp",
+      author:" Harper Lee ",
+      description:"A lawyer's advice to his children as he defends the real mockingbird of Harper Lee's classic novel - a black man falsely charged with the rape of a white girl. Through the young eyes of Scout and Jem Finch, Harper Lee explores with exuberant humour the irrationality of adult attitudes to race and class in the Deep South of the 1930s."
+
+
+    },
+    {
+        id:"7",
+        name:"Animal Farm",
+        image:"https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781839642395/animal-farm-9781839642395_lg.jpg",
+        author:"Geroge Barwell",
+        description:"Animal Farm is a moral animal fable written to highlight the weakness of humankind, and satirize the rule of Stalin, whose rise through revolution ended in totalitarianism. Peppered with slogans such as 'All Animals Are Equal', Orwell undermines the dark treachery of the pigs with a simple economy of style."
+
+    }
+]
